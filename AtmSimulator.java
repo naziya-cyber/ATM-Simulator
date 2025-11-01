b6eee3709888b01eae0dacc61f17d13eb5605fce
nazia <naziya222005@gmail.com>
2025-11-01 23:15:25 +0530

    Second

--- AtmSimulator.java
+++ AtmSimulator.java
@@ -12,8 +12,9 @@ public class AtmSimulator {
 
         System.out.println("______________________");
         System.out.println("ATM SIMULATOR");
-        System.out,println("______________________");
-
+        System.out.println("______________________");
+        
+        while(isWorking){
         System.out.println("Features:");
         System.out.println("1. Show The Bank Account Balance!");
         System.out.println("2. Deposite Your Money!");
@@ -21,27 +22,28 @@ public class AtmSimulator {
         System.out.println("Exite!");
 
         System.out.print("Enter Your Choice from (1) to (4) : ");
-        userChoice = sc.nextDouble();
+        userChoice = sc.nextInt();
         
-
         switch(userChoice){
-            case 1 : 
-              System.out.println("1. Show The Bank Account Balance!");
+          case 1 : 
+          System.out.println("1. Show The Bank Account Balance!");
 
             case 2 :
              System.out.println("2. Deposite Your Money!");
 
             case 3 :
               System.out.println("3. Withdrowl Your Money!");
-
-            case 4 :
+              
+              case 4 :
               System.out.println("Exite!");
-
+              
             default : System.out.println("Invalid Number !!");
         }
         
-
-
+      }
+      
+        
+        
 
 
         sc.close();
